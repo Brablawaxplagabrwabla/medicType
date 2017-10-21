b2262274c64b4b6ed9cacbd767d86505cc01cc34
brablawaxplagabrwabla <gtroncone@correo.unimet.edu.ve>
2017-10-21 19:33:34 -0400

    conexion local

--- webserver/server.js
+++ webserver/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 var port = 3000;
 
 app.use(express.static('medicType'));
+
+var connect = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '5899',
+	database: 'MedicTypeDB'
+});
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/medicType/index.html');
